71ba1c2a32f2cc2cb7e973eab0d20ef9f07ebdb4
Matěj Neděla <nedela.matej@gmail.com>
2025-04-11 23:44:17 +0200

    chore: install `cors-bundle` library

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,7 @@
 return [
   Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
   Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+  Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
   Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
   Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
   Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
